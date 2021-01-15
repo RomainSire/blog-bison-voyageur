809455468c8c9bcd6154be2627ac90a139b3fab5
Romain <romain.sire@gmx.com>
2021-01-15 21:40:05 +0100

    add unique valifator

--- backend/src/models/Article.js
+++ backend/src/models/Article.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
+const uniqueValidator = require('mongoose-unique-validator');
+
 const articleSchema = mongoose.Schema({
   created_at: { type: Date, required: true },
   modified_at: { type: Date, required: true },
-  title: { type: String, required: true },
-  slug: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
+  slug: { type: String, required: true, unique: true },
   image_id: { type: Number, required: false },
   content: { type: String, required: false },
   isdraft: { type: Boolean, required: true }
 });
+
+articleSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Article', articleSchema);
