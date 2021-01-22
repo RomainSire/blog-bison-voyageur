70a2a94b2b3406de7fe73aaf4cbd6045dbc221c6
Romain <romain.sire@gmx.com>
2021-01-22 22:27:28 +0100

    add & test route POST api/auth/login

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,19 +1,17 @@
 const bcrypt = require('bcrypt');
+const cryptojs = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const Cookies = require('cookies');
+
 const User = require('../models/User');
+const userUtils = require('../utils/userUtils');
 
 
 exports.createFirstUser = async (req, res, next) => {
   try {
-    const DBUsers = await User.find()
+    const DBUsers = await User.find();
     if (DBUsers.length === 0) {
-      const hash = await bcrypt.hash('admin', 12);
-      const now = new Date();
-      const user = new User({
-        username: 'admin',
-        hash: hash,
-        created_at: now,
-        modified_at: now
-      });
+      const user = await userUtils.generateUserForDB('admin', 'admin');
       const savedUser = await user.save()
       return res.status(201).json({ ...savedUser._doc, message: 'utilisateur par défaut créé' });
     } else {
@@ -24,8 +22,45 @@ exports.createFirstUser = async (req, res, next) => {
   }
 }
 
-exports.login = (req, res ,next) => {
-
+exports.login = async (req, res ,next) => {
+  try {
+    // Recherche user
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json({ error: 'Utilisateur invalide' });
+    }
+    // décryptage & validation du hash
+    const databaseHash = cryptojs.AES.decrypt(user.hash, process.env.KEY_PASSWORD).toString(cryptojs.enc.Utf8);
+    const valid = await bcrypt.compare(req.body.password, databaseHash);
+    if (!valid) {
+      return res.status(401).json({ error: 'Mot de passe invalide' });
+    }
+    // Création d'un JWT
+    const newToken = jwt.sign(
+      { userId: user._id },
+      process.env.KEY_JWT,
+      { expiresIn: '4h' }
+    );
+    // Création du contenu du cookie avec le JWT
+    const cookieContent = {
+      token: newToken,
+      userId: user._id
+    };
+    // Cryptage du contenu du cookie avant de l'envoyer
+    const cryptedCookie = cryptojs.AES.encrypt(JSON.stringify(cookieContent), process.env.KEY_COOKIE).toString();
+    new Cookies(req, res).set('cryptedToken', cryptedCookie, {
+      httpOnly: true,
+      maxAge: 14400000  // cookie pendant 4 heures
+    })
+    // Envoi de la réponse
+    res.status(200).json({
+      message: 'Utilisateur loggé',
+      userId: user._id,
+      username: user.username
+    })
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 }
 
 exports.changeUsername = (req, res, next) => {
--- backend/test/userSpec.js
+++ backend/test/userSpec.js
@@ -10,6 +10,7 @@ chai.use(chaiHttp);
 
 const app = require('../src/app')
 const User = require('../src/models/User');
+const userUtils = require('../src/utils/userUtils')
 
 describe('User', () => {
   // DELETE EVERYTHING FROM THE TEST DATABASE BEFORE EVERY SINGLE TEST
@@ -50,14 +51,92 @@ describe('User', () => {
       user.save()
         .then(() => {
           chai.request(app)
-        .get('/api/auth/firstuser')
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.a('object');
-          res.body.should.have.property('message').eql('Des utilisateurs existent déjà');
-          done();
-        })
+          .get('/api/auth/firstuser')
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message').eql('Des utilisateurs existent déjà');
+            done();
+          });
         })
     });
-  })
+  });
+
+
+  /**************************************************************************
+   * Test the POST /api/auth/login route
+   **************************************************************************/
+  describe('POST /api/auth/login', () => {
+    it('should not connect if there is no user in DB', (done) => {
+      const credential = {
+        username: 'JohnDoe',
+        password: 'monmotdepasse'
+      };
+      chai.request(app)
+        .post('/api/auth/login')
+        .send(credential)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error').eql('Utilisateur invalide');
+          done();
+        });
+    });
+    it('should connect and get a cookie', async () => {
+      const user = await userUtils.generateUserForDB('admin', 'password');
+      const savedUser = await user.save();
+      const credential = {
+        username: 'admin',
+        password: 'password'
+      };
+      chai.request(app)
+        .post('/api/auth/login')
+        .send(credential)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Utilisateur loggé');
+          res.body.should.have.property('userId');
+          res.body.userId.toString().should.eql(savedUser._id.toString());
+          res.body.should.have.property('username').eql('admin');
+          // Vérification: token dans cookie
+          res.headers.should.be.a('object')
+          res.headers.should.have.property('set-cookie');
+          res.headers['set-cookie'].should.be.a('array');
+          res.headers['set-cookie'].pop().split(';')[0].split('=')[0].should.eql('cryptedToken');
+        })
+    });
+    it('should not connect if username is wrong', async () => {
+      const user = await userUtils.generateUserForDB('admin', 'password');
+      await user.save();
+      const credential = {
+        username: 'badlogin',
+        password: 'password'
+      };
+      chai.request(app)
+        .post('/api/auth/login')
+        .send(credential)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error').eql('Utilisateur invalide');
+        })
+    });
+    it('should not connect if password is wrong', async () => {
+      const user = await userUtils.generateUserForDB('admin', 'password');
+      await user.save();
+      const credential = {
+        username: 'admin',
+        password: 'badPassword'
+      };
+      chai.request(app)
+        .post('/api/auth/login')
+        .send(credential)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error').eql('Mot de passe invalide');
+        })
+    });
+  });
 })
